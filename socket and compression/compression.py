0f98087e719d1d5db35be4c77f4b3cbedef0a41c
HUANGRH <renhuan@hotmail.com>
2022-06-04 01:41:58 +0800

    Add files via upload

---
+++ socket and compression/compression.py
@@ -0,0 +1,24 @@
+import tarfile
+from tqdm import tqdm # pip3 install tqdm
+
+def compress(tar_file, members):
+    """
+    Adds files (`members`) to a tar_file and compress it
+    """
+    # open file for gzip compressed writing
+    tar = tarfile.open(tar_file, mode="w:gz")
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        # add file/folder/link to the tar file (compress)
+        tar.add(member)
+        # set the progress description of the progress bar
+        progress.set_description(f"Compressing {member}")
+    # close the file
+    tar.close()
+
+
+# now test compressing files
+
+compress("kindle.tar.gz",["C:\\Users\HRH\\Documents\\Kindle Content","client.py"])
---
+++ socket and compression/decompression.py
@@ -0,0 +1,28 @@
+
+import tarfile
+from tqdm import tqdm # pip3 install tqdm
+
+def decompress(tar_file, path, members=None):
+    """
+    Extracts `tar_file` and puts the `members` to `path`.
+    If members is None, all members on `tar_file` will be extracted.
+    """
+    tar = tarfile.open(tar_file, mode="r:gz")
+    if members is None:
+        members = tar.getmembers()
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        tar.extract(member, path=path)
+        # set the progress description of the progress bar
+        progress.set_description(f"Extracting {member.name}")
+    # or use this
+    # tar.extractall(members=members, path=path)
+    # close the file
+    tar.close()
+
+
+# now let's decompress it
+
+decompress("kindle.tar.gz", "extracted")
